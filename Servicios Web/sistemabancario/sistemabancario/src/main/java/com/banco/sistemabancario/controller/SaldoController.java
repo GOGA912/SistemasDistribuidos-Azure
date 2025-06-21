57cebbac311f5d1db3cc53614eebca202ad56b1c
Yael Galvan <168500000+GOGA912@users.noreply.github.com>
2025-06-20 20:52:09 -0600

    Update SaldoController.java

--- Servicios Web/sistemabancario/sistemabancario/src/main/java/com/banco/sistemabancario/controller/SaldoController.java
+++ Servicios Web/sistemabancario/sistemabancario/src/main/java/com/banco/sistemabancario/controller/SaldoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/saldo")
+@CrossOrigin(origins = "http://localhost:8000")
 public class SaldoController {
 
     @Autowired
