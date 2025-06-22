6a2c88a5d77b978c83787d8fde2c97aaf248b0bd
Yael Galvan <168500000+GOGA912@users.noreply.github.com>
2025-06-22 01:51:34 -0600

    Update LoginService.java

--- Microservicios/Microservicios/Microservicios/src/main/java/com/banco/microservicios/LoginService.java
+++ Microservicios/Microservicios/Microservicios/src/main/java/com/banco/microservicios/LoginService.java
@@ -7,7 +7,9 @@ public class LoginService {
     public static void main(String[] args) {
         try {
             // Crear el servidor en puerto 8081
-            HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+            
+            int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
             // Asociar la ruta /login con el manejador
             server.createContext("/login", new LoginHandler());
@@ -16,7 +18,7 @@ public class LoginService {
             server.setExecutor(null); // Usa el executor por defecto
             server.start();
 
-            System.out.println("Servidor de autenticación iniciado en http://localhost:8081/login");
+            System.out.println("Servidor de autenticación iniciado en http://localhost:"+port+"/login");
         } catch (Exception e) {
             System.out.println("Error al iniciar el servidor: " + e.getMessage());
         }
