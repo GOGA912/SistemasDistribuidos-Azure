246d3fd1faaa8f6ba1368facf91d3ab0d8cd59ee
GOGA912 <galvan30@live.com.x>
2025-06-22 19:24:20 -0600

    Microservicios

--- Microservicios/Microservicios/Microservicios/movementes-service/src/main/java/com/banco/microservicios/movementsService/MovementsService.java
+++ Microservicios/Microservicios/Microservicios/movementes-service/src/main/java/com/banco/microservicios/movementsService/MovementsService.java
@@ -6,7 +6,8 @@ import java.net.InetSocketAddress;
 public class MovementsService {
     public static void main(String[] args) {
         try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8085), 0);
+            int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
             server.createContext("/movimientos", new MovimientosHandler());
             server.setExecutor(null); 
             server.start();
